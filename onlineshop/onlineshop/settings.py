6194efcacbe92fab8838948ef4e5592fe2be49c8
hamet <taheri.hamed70@gmail.com>
2025-07-28 16:05:08 +0330

    config database postgersql

--- onlineshop/onlineshop/settings.py
+++ onlineshop/onlineshop/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'onlineshop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': BASE_DIR / 'test',
+        'USER': 'postgres',
+        'PASSWORD': '<PASSWORD>',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
