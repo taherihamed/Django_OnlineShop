0d22d5b29fe7a26f82e903405bf8e019dde6f699
hamet <taheri.hamed70@gmail.com>
2025-07-28 11:38:14 +0330

    add user model

--- onlineshop/user/models.py
+++ onlineshop/user/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=20, unique=True)
+    password = models.CharField(max_length=128)
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=50, blank=True)
+    last_name = models.CharField(max_length=50, blank=True)
+    address = models.TextField()
+    city = models.CharField(max_length=50)
+    zipcode = models.CharField(max_length=10)
+    phone_number = models.CharField(max_length=20)
+
+
+def __str__(self):
+    return self.username
+
+
+
+
